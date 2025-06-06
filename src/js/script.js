55aec70f2ac53acb730f10d77af3088bc8bb9397
Hanspeter Dietiker <hansdietiker2012@gmail.com>
2025-06-06 14:12:57 -0300

    feat: implemented isSafeToMove, renderMaze funcs

--- src/js/script.js
+++ src/js/script.js
@@ -17,19 +17,53 @@ function solveMaze(currentRow, currentCol) {
         return true;
     }
 
-     if (isSafeToMove(currentRow, currentCol)) {
+    if (isSafeToMove(currentRow, currentCol)) {
         solutionMatrix[currentRow][currentCol] = 1;
 
-    
+
         if (solveMaze(currentRow - 1, currentCol)) return true; // Cima
         if (solveMaze(currentRow, currentCol + 1)) return true; // Direita
         if (solveMaze(currentRow + 1, currentCol)) return true; // Baixo
         if (solveMaze(currentRow, currentCol - 1)) return true; // Esquerda
 
-        
+
         solutionMatrix[currentRow][currentCol] = 0;
         return false;
     }
-        return false;
+    return false;
 }
 
+function isSafeToMove(row, col) {
+    const isInsideMaze = row >= 0 && row < mazeRows && col >= 0 && col < mazeCols;
+    const isFreePath = mazeMatrix[row][col] === 0;
+    const isNotVisited = solutionMatrix[row][col] === 0;
+
+    return isInsideMaze && isFreePath && isNotVisited;
+}
+
+function renderMaze() {
+    const mazeContainer = document.getElementById('maze');
+    mazeContainer.classList.add('maze');
+    mazeContainer.style.gridTemplateColumns = `repeat(${mazeCols}, 30px)`;
+    mazeContainer.innerHTML = '';
+
+    for (let row = 0; row < mazeRows; row++) {
+        for (let col = 0; col < mazeCols; col++) {
+            const cellElement = document.createElement('div');
+            cellElement.classList.add('cell');
+
+            if (mazeMatrix[row][col] === 1) {
+                cellElement.classList.add('wall');
+            } else {
+                cellElement.classList.add('path');
+            }
+
+            if (solutionMatrix[row][col] === 1) {
+                cellElement.classList.add('solution');
+                cellElement.textContent = '■';
+            }
+
+            mazeContainer.appendChild(cellElement);
+        }
+    }
+}
